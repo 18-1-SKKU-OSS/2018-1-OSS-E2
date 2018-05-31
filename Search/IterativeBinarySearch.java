b73e532bbc91dfe1e467abf076c74872c4730543
hong skkusosc <--hsp5125@naver.com>
2018-05-31 10:07:16 +0900

    edited version
    Signed-off-by: hong skkusosc <--hsp5125@naver.com>

--- Search/IterativeBinarySearch.java
+++ Search/IterativeBinarySearch.java
@@ -7,15 +7,15 @@ import java.util.stream.Stream;
 import static java.lang.String.format;
 
 /**
- * Binary search is one of the most popular algorithms
- * This class represents iterative version  {@link BinarySearch}
- * Iterative binary search is likely to have lower constant factors because it doesn't involve the overhead of manipulating the call stack.
- * But in java the recursive version can be optimized by the compiler to this version.
+ * 이진 검색은 가장 널리 사용되는 알고리즘 중 하나입니다.
+?*이 클래스는 반복 버전을 나타냅니다. BinarySearch
+?* 반복적 인 이진 검색은 호출 스택을 조작하는 오버 헤드를 포함하지 않기 때문에 상수 요소가 더 낮을 수 있습니다.
+?* 그러나 자바에서 재귀 버전은이 버전의 컴파일러에 의해 최적화 될 수 있습니다.
  *
- * Worst-case performance	O(log n)
- * Best-case performance	O(1)
- * Average performance	O(log n)
- * Worst-case space complexity	O(1)
+ * 최악의 경우	O(log n)
+ * 최상의 경우	O(1)
+ * 평균	O(log n)
+ * 최악의 경우 공간복잡도	O(1)
  *
  * @author Gabriele La Greca : https://github.com/thegabriele97
  * @author Podshivalov Nikita (https://github.com/nikitap492)
@@ -30,8 +30,8 @@ public final class IterativeBinarySearch implements SearchAlgorithm {
     /**
      * This method implements an iterative version of binary search algorithm
      * 
-     * @param array a sorted array
-     * @param key the key to search in array
+     * @param array는 정렬된 배열입니다
+     * @param key는 찾아야하는 값입니다.
      * 
      * @return the index of key in the array or -1 if not found
      */
