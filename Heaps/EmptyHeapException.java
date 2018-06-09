006c0ad5da0baabc7fa0c4e015b202761284c80b
Park jiyeol <38033175+Goodbam@users.noreply.github.com>
2018-06-09 22:55:00 +0900

    complete translation
    haha it works well in web

--- Heaps/EmptyHeapException.java
+++ Heaps/EmptyHeapException.java
@@ -6,7 +6,8 @@ package heaps;
 /**
  * @author Nicolas Renard
  * Exception to be thrown if the getElement method is used on an empty heap.
- *
+ * 저자 Nicloas Renard
+ * getElement가 어떤 원소도 갖지 않는 힙 구조에서 사용될 경우, 넘겨질 예외
  */
 @SuppressWarnings("serial")
 public class EmptyHeapException extends Exception {
